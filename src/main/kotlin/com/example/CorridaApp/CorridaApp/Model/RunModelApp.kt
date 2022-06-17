54a9886608312356a05df3711441351f3b9eea2c
Valter Gabriel <vgabrielbri@hotmail.com>
2022-06-17 10:01:03 -0300

    Trocando isday e iscity de boolean para string

--- src/main/kotlin/com/example/CorridaApp/CorridaApp/Model/RunModelApp.kt
+++ src/main/kotlin/com/example/CorridaApp/CorridaApp/Model/RunModelApp.kt
@@ -18,8 +18,8 @@ data class RunModelApp(
     val avergedSpeed: Double = 0.00,
     val totalDistance: Double = 0.00,
     val note: String = "",
-    val isDay: Boolean = true,
-    val isCity: Boolean = true,
+    val isDay: String = "",
+    val isCity: String = "",
     val dateRun: String = "",
     val imgRun : ByteArray? = null
 )
